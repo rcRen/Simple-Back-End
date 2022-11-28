52d8b4834fd2e599dfd683b90e3313c388dd0084
Rucheng <rrc@s-3.local>
2022-11-28 13:36:50 -0500

    connect to front-end

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const userModel = require("./models");
+mongoose.connect("mongodb+srv://rrc:" + process.env.MONGODB_PWD + "@cluster0.vvv4rhf.mongodb.net/myFirstDb?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); //cross-orign resource sharing
@@ -8,5 +21,10 @@ app.use(express.json()); // Allows express to read a request body
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/users', async (req, res) => {
+    const users = await userModel.find();
+    res.send(users);
+});
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
